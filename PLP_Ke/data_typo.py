c28787e83de00db764d38f958739c5c7d8b3761a
ke411 <kennedyouma784@gmail.com>
2024-03-01 16:41:14 +0300

    Printing out personalized message

---
+++ PLP_Ke/data_typo.py
@@ -0,0 +1,14 @@
+a=100
+b=32
+c=3.14
+print(a+b * 2)
+print((a+b)*2)
+print(a==b)
+print(a>b)
+d=12+2j
+
+plp_str= "Welcome To PLP Academy"
+print(plp_str)
+print(plp_str[0])
+print(plp_str[-1])
+print(plp_str[13:15])
